dbb898b12d4b076c0395ea8f432e49d69e9cfe0c
cuijunsheng <744612262@qq.com>
2020-04-21 07:37:57 +0800

    布局的基本构建

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,67 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
+/* Layout */
+import Layout from '@/layout/Layout'
+
+
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/home',
+    children: [{
+      path: '/home',
+      component: () => import('views/home/Home'),
+      meta: { title: 'Home', icon: 'dashboard' }
+    }]
+  },
+  {
+    path: '/about',
+    component: Layout,
+    children: [{
+      path: '/index',
+      component: () => import('views/about/About'),
+      meta: { title: 'About', icon: 'example' }
+    }]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
 })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
 
 export default router
---
+++ src/views/about/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h2>222222222222</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+
+</style>
